b3647f753759d66193f4ac8d72dddc5cc5722f79
Leonardo Lobas <leonardolobas2703@gmail.com>
2024-09-30 22:51:13 -0300

    Versao final.

--- RM560041_EX01.py
+++ RM560041_EX01.py
@@ -10,16 +10,23 @@ contadores = {
     'sexta-feira': 0
 }
 
-
-while (colabNum < colaboradores):
-    diaEscolhido = input('Informe o dia de sua preferência(segunda-feira,terça-feira,quarta-feira,quinta-feira,sexta-feira):')
-    
+while colabNum < colaboradores:
+    diaEscolhido = input('Informe o dia de sua preferência (segunda-feira, terça-feira, quarta-feira, quinta-feira, sexta-feira): ').strip().lower()
     
     if diaEscolhido in contadores:
         contadores[diaEscolhido] += 1
-        colabNum+= 1
+        colabNum += 1
     else: 
-        print('Dia invalido!')
-diaMaisEscolhido = max(contadores,key=contadores.get)
-print(f'O dia escolhido pelos colaboradores é :{diaMaisEscolhido}')
+        print('Dia inválido!')
 
+
+max_votos = max(contadores.values())
+
+
+dias_mais_escolhidos = [dia for dia, votos in contadores.items() if votos == max_votos]
+
+
+if len(dias_mais_escolhidos) > 1:
+    print(f"Há um empate entre os seguintes dias: {', '.join(dias_mais_escolhidos)}")
+else:
+    print(f"O dia mais escolhido pelos colaboradores é: {dias_mais_escolhidos[0]}")
